6a13b013f169c221738da4cb03776ed7808323e0
fxy <962454244@qq.com>
2022-02-24 22:05:15 +0800

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -79,8 +79,9 @@ public class Percolation {
     public boolean percolates() {
         return uf.connected(top, bottom);
     }
-/**
+
     public static void main(String[] args) {
+        /**
         Percolation p = new Percolation(5);
         p.open(2, 1);
         p.open(3, 2);
@@ -96,5 +97,6 @@ public class Percolation {
         System.out.println(p.isFull(3, 3)); // true
         p.open(4, 3);
         System.out.println(p.percolates()); // true
-    }*/
+         */
+    }
 }
